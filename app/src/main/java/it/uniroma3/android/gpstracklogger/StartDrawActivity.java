9408f2d86769464bc4eaded654bcd54dcb22ea78
Fabio Cibecchini <fabio.cibecchini@gmail.com>
2015-05-21 11:24:36 +0200

    update

--- app/src/main/java/it/uniroma3/android/gpstracklogger/StartDrawActivity.java
+++ app/src/main/java/it/uniroma3/android/gpstracklogger/StartDrawActivity.java
@@ -5,35 +5,27 @@ import android.graphics.Color;
 import android.os.Bundle;
 import android.view.MotionEvent;
 import android.view.ScaleGestureDetector;
-import android.view.ViewGroup;
-import android.widget.FrameLayout;
 
 import it.uniroma3.android.gpstracklogger.views.DrawView;
 
 
 public class StartDrawActivity extends Activity {
     private DrawView drawView;
-    ViewGroup root;
     private float lastX, lastY;
     private ScaleGestureDetector mScaleDetector;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_draw);
         mScaleDetector = new ScaleGestureDetector(getApplicationContext(), new ScaleListener());
-        root = (ViewGroup) findViewById(R.id.linearlayout1);
         drawView = new DrawView(this);
         drawView.setBackgroundColor(Color.WHITE);
-        FrameLayout.LayoutParams layoutParams = new FrameLayout.LayoutParams(FrameLayout.LayoutParams.MATCH_PARENT, FrameLayout.LayoutParams.MATCH_PARENT);
-        drawView.setLayoutParams(layoutParams);
-        root.addView(drawView);
+        setContentView(drawView);
     }
 
     @Override
     public boolean onTouchEvent(MotionEvent event) {
         if (event.getPointerCount() > 1) {
-
             switch (event.getAction()) {
                 case MotionEvent.ACTION_DOWN: {
                     final float x = event.getX();
